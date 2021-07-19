f3cac0fc792388c8de50638b97228bc66c6aa0ee
Octave Bird <85306435+0xbirdy@users.noreply.github.com>
2021-07-19 11:13:36 +0200

    Update

--- hot-summer.py
+++ hot-summer.py
@@ -3,22 +3,22 @@ import moviepy.editor as mpy
 import numpy as np
 import sys
 
-W,H = 1024,1024 # width, height, in pixels
+width,height = 1024,1024 # in pixels
 colors = [(255/255, 169/255, 0), (255/255, 118/255, 0), (205/255, 17/255, 59/255), (82/255, 0, 106/255)]
 audio = mpy.AudioFileClip(sys.argv[1])
 n_circles = 50
 
 def make_frame(t):
-  surface = gz.Surface(W,H)
+  surface = gz.Surface(width,height)
 
   for i in range(n_circles):
     angle = 2*np.pi*(1.0*i/n_circles+t/10)
-    center = W*( 0.5+ gz.polar2cart(0.1,angle))
-    circle = gz.circle(r=W*(1.0-1.0*i/n_circles), xy=center, fill=colors[i % len(colors)])
+    center = width*( 0.5+ gz.polar2cart(0.1,angle))
+    circle = gz.circle(r=width*(1.0-1.0*i/n_circles), xy=center, fill=colors[i % len(colors)])
     circle.draw(surface)
 
-  contour1 = gz.circle(r=.65*W,xy=[W/2,W/2], stroke_width=.5*W, stroke=(0,0,0))
-  contour2 = gz.circle(r=.42*W,xy=[W/2,W/2], stroke_width=.02*W, stroke=colors[0])
+  contour1 = gz.circle(r=.65*width,xy=[width/2,width/2], stroke_width=.5*width, stroke=(0,0,0))
+  contour2 = gz.circle(r=.42*width,xy=[width/2,width/2], stroke_width=.02*width, stroke=colors[0])
   contour1.draw(surface)
   contour2.draw(surface)
   return surface.get_npimage()
